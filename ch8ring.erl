e61afad013dde0dfd3c0145cdf633807cbceca97
Benson Wong <mostlygeek@gmail.com>
2011-07-31 16:44:13 -0700

    add benchmarch to ch8 ring code

--- ch8ring.erl
+++ ch8ring.erl
@@ -4,28 +4,39 @@
 %% for different values of N and M.
 
 -module(ch8ring).
--export([create/1]).
+-export([create/1,bench/2]).
+
+-ifdef(debug).
+-define(DEBUG(X,Y), io:format(X,Y)).
+-else.
+-define(DEBUG(X,Y), void).
+-endif.
+
+bench(N, M) ->
+    First = create(N),
+    statistics(wall_clock),
+    First ! {send, t, M},
+    io:format("Start ~p processes, ~p rounds = ~p msg passes.~n",[N,M,N*M]).
 
 % make a ring with N nodes
 create(N) ->
     First = spawn(fun() -> loop(defer) end),
-    io:format(" - Created first node: ~p~n", [First]),    
+    ?DEBUG(" - Created first node: ~p~n", [First]),    
     create(N-1, First, First),    
     First.
     
-
 create(0, First, PassTo) ->
-    io:format("msg to ~p to set passTo to ~p~n",[First,PassTo]),
+    ?DEBUG("msg to ~p to set passTo to ~p~n",[First,PassTo]),
     First ! {passTo, PassTo};
     
 create(N, First, PassTo) ->
     Pid = spawn(fun() -> loop(PassTo) end),
-    io:format(" - Created ~p with pass to ~p~n", [Pid, PassTo]),
+    ?DEBUG(" - Created ~p with pass to ~p~n", [Pid, PassTo]),
     create(N-1, First, Pid). 
 
 loop(PassTo) ->
     receive        
-        % set the next. used when we're at the end.
+        % set the new PassTo, used to link process N to process 1.
         {passTo, NewPassTo} ->
             loop(NewPassTo);
             
@@ -33,28 +44,32 @@ loop(PassTo) ->
         {send, Msg, M} -> 
             case (is_pid(PassTo)) of 
                 true -> 
-                    io:format("~p got message~n",[self()]),
+                    ?DEBUG("~p got message~n",[self()]),
                     PassTo ! {pass, self(), Msg, M, 0};
                 false -> false
             end, 
             loop(PassTo);
         
+        % pass message along
         {pass, Origin, Msg, M, Count} ->
             if 
                 Origin =:= self() andalso Count < M ->
-                    io:format("  DONE ROUND ~p of ~p~n",[Count+1,M]),
-                    io:format("  > ~p pass [~p] to ~p~n",[self(), Msg, PassTo]),
+                    ?DEBUG("  DONE ROUND ~p of ~p~n",[Count+1,M]),
+                    ?DEBUG("  > ~p pass [~p] to ~p~n",[self(), Msg, PassTo]),
                     PassTo ! {pass, Origin, Msg, M, Count+1};
                 Origin =:= self() andalso Count =:= M ->
-                    io:format("  All Done!.~n");
+                    {_, Time} = statistics(wall_clock),
+                    io:format("Done. Time: ~p seconds!~n", [Time/1000]),
+                    Origin ! stop;                    
                 true ->
-                    io:format("  > ~p pass [~p] to ~p~n",[self(), Msg, PassTo]),
+                    ?DEBUG("  > ~p pass [~p] to ~p~n",[self(), Msg, PassTo]),
                     PassTo ! {pass, Origin, Msg, M, Count}
             end,
             loop(PassTo);
-
+    
+        % send a stop command down the line
         stop ->
-            io:format("  ~p stopping~n",[self()]),
+            ?DEBUG("  ~p stopping~n",[self()]),
             PassTo ! stop,
             true
-    end.    
+    end.
